45a2b4c1f00cc16f5f4834c69006190e67d59a71
Dharossantana <dharossantana2000@gmail.com>
2021-04-03 11:57:22 +0800

    Upload Kedua Portofolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,22 +11,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="dharos/assets/img/favicon.png" rel="icon">
-  <link href="dharos/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="portofolio/assets/img/favicon.png" rel="icon">
+  <link href="portofolio/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="dharos/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="dharos/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="dharos/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="dharos/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="dharos/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="dharos/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="portofolio/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="dharos/assets/css/style.css" rel="stylesheet">
+  <link href="portofolio/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Personal - v4.1.0
@@ -42,9 +42,9 @@
   <header id="header">
     <div class="container">
 
-      <h1><a href="index.html">I Gde Dharos Santana Darma</a></h1>
+      <h1><a href="index.html">I Gde portofolio Santana Darma</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="mr-auto"><img src="dharos/assets/img/logo.png" alt="" class="img-fluid"></a> -->
+      <!-- <a href="index.html" class="mr-auto"><img src="portofolio/assets/img/logo.png" alt="" class="img-fluid"></a> -->
       <h2>saya kuliah di Universitas Bumigora Mataram Jurusan Ilmu Komputer</h2>
 
       <nav id="navbar" class="navbar">
@@ -78,7 +78,7 @@
 
       <div class="row">
         <div class="col-lg-4" data-aos="fade-right">
-          <img src="dharos/assets/img/me.jpg" class="img-fluid" alt="">
+          <img src="portofolio/assets/img/me.jpg" class="img-fluid" alt="">
         </div>
         <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
           <h3WEb Designer></h3>
@@ -97,7 +97,7 @@
             <div class="col-lg-6">
               <ul>
                 <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>21</span></li>
-                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>dharossantana2000@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>portofoliosantana2000@gmail.com</span></li>
               </ul>
             </div>
           </div>
@@ -323,12 +323,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>App 1</h4>
               <p>App</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -342,7 +342,7 @@
               <h4>Web 3</h4>
               <p>Web</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -351,12 +351,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>App 2</h4>
               <p>App</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -365,12 +365,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>Card 2</h4>
               <p>Card</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -379,12 +379,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>Web 2</h4>
               <p>Web</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -393,12 +393,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>App 3</h4>
               <p>App</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -407,12 +407,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>Card 1</h4>
               <p>Card</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -421,12 +421,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>Card 3</h4>
               <p>Card</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -435,12 +435,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap">
-            <img src="dharos/assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+            <img src="portofolio/assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>Web 3</h4>
               <p>Web</p>
               <div class="portfolio-links">
-                <a href="dharos/assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="portofolio/assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -532,20 +532,20 @@
     <!-- You can delete the links only if you purchased the pro version. -->
     <!-- Licensing information: https://bootstrapmade.com/license/ -->
     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-resume-bootstrap-template/ -->
-    Designed by <a href="https://www.instagram.com/ Dharos santana">Dharos santana</a>
+    Designed by <a href="https://www.instagram.com/ portofolio santana">portofolio santana</a>
   </div>
 
   <!-- Vendor JS Files -->
-  <script src="dharos/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="dharos/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="dharos/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="dharos/assets/vendor/php-email-form/validate.js"></script>
-  <script src="dharos/assets/vendor/purecounter/purecounter.js"></script>
-  <script src="dharos/assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="dharos/assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="portofolio/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="portofolio/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="portofolio/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="portofolio/assets/vendor/php-email-form/validate.js"></script>
+  <script src="portofolio/assets/vendor/purecounter/purecounter.js"></script>
+  <script src="portofolio/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="portofolio/assets/vendor/waypoints/noframework.waypoints.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="dharos/assets/js/main.js"></script>
+  <script src="portofolio/assets/js/main.js"></script>
 
 </body>
 
